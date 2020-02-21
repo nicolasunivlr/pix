f1ced1f4ff21a6e826771f5a2182fc3809744273
celineung <celine.ung@zenika.com>
2020-02-21 17:41:33 +0100

    WIP

---
+++ mon-pix/app/components/button.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class Button extends Component {
+  title = "nouveau titre";
+
+  // @action
+  // executeAction() {
+  //   console.log("log log");
+  // }
+}
