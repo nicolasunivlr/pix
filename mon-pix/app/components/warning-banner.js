39ff0f1a66af5aae02fd14419056aeb5f4018a69
celineung <celine.ung@zenika.com>
2020-04-17 17:44:02 +0200

    Add prod banner if problems

--- mon-pix/app/components/warning-banner.js
+++ mon-pix/app/components/warning-banner.js
@@ -2,5 +2,10 @@ import Component from '@glimmer/component';
 import ENV from 'mon-pix/config/environment';
 
 export default class WarningBanner extends Component {
-  isEnabled = ENV.APP.IS_STAGING_BANNER_ENABLED;
+  isStagingBannerEnabled = ENV.APP.IS_STAGING_BANNER_ENABLED;
+  isProdProblemBannerEnabled = ENV.APP.IS_PROD_HAVING_PROBLEMS_BANNER_ENABLED;
+
+  get displayBanner() {
+    return this.isStagingBannerEnabled || this.isProdProblemBannerEnabled;
+  }
 }
--- mon-pix/tests/integration/components/warning-banner-test.js
+++ mon-pix/tests/integration/components/warning-banner-test.js
@@ -9,14 +9,17 @@ describe('Integration | Component | warning-banner', function() {
   setupRenderingTest();
 
   const originalIsStagingBannerEnabled = ENV.APP.IS_STAGING_BANNER_ENABLED;
+  const originalIsProdProblemBannerEnabled = ENV.APP.IS_PROD_HAVING_PROBLEMS_BANNER_ENABLED;
 
   afterEach(function() {
     ENV.APP.IS_STAGING_BANNER_ENABLED = originalIsStagingBannerEnabled;
+    ENV.APP.IS_PROD_HAVING_PROBLEMS_BANNER_ENABLED = originalIsProdProblemBannerEnabled;
   });
 
   it('should not display the banner when not in staging', async function() {
     // given
     ENV.APP.IS_STAGING_BANNER_ENABLED = false;
+    ENV.APP.IS_PROD_HAVING_PROBLEMS_BANNER_ENABLED = false;
 
     // when
     await render(hbs`<WarningBanner />`);
@@ -28,11 +31,28 @@ describe('Integration | Component | warning-banner', function() {
   it('should display the banner when in staging', async function() {
     // given
     ENV.APP.IS_STAGING_BANNER_ENABLED = true;
+    ENV.APP.IS_PROD_HAVING_PROBLEMS_BANNER_ENABLED = false;
 
     // when
     await render(hbs`<WarningBanner />`);
 
     // then
     expect(find('.warning-banner')).to.exist;
+    expect(find('.warning-banner__staging-text')).to.exist;
+    expect(find('.warning-banner__problem-text')).to.not.exist;
+  });
+
+  it('should display the banner when having problem in production', async function() {
+    // given
+    ENV.APP.IS_STAGING_BANNER_ENABLED = false;
+    ENV.APP.IS_PROD_HAVING_PROBLEMS_BANNER_ENABLED = true;
+
+    // when
+    await render(hbs`<WarningBanner />`);
+
+    // then
+    expect(find('.warning-banner')).to.exist;
+    expect(find('.warning-banner__staging-text')).to.not.exist;
+    expect(find('.warning-banner__prod-problem-text')).to.exist;
   });
 });
